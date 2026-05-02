fbe2ea82615909f5e80ecf5a74786d6ca253640e
Garegin Yervandyan <garegin.y.g@gmail.com>
2026-05-02 20:28:25 +0400

    translating admin page

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -3,9 +3,13 @@ import { useEffect, useState } from "react";
 import closeEye from "../../assets/closeEye.svg";
 import authStore, { login } from "../../stores/admin_store.js";
 import { useNavigate } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
 const Login = () => {
+  const { t } = useTranslation();
+
   const [showPassword, setShowPassword] = useState(false);
+
   const [userData, setUserData] = useState({
     username: "",
     password: "",
@@ -13,6 +17,7 @@ const Login = () => {
 
   const navigate = useNavigate();
   const token = localStorage.getItem("access_token");
+
   const error = authStore((state) => state.error);
   const loading = authStore((state) => state.loading);
 
@@ -20,11 +25,13 @@ const Login = () => {
     if (token) {
       navigate("/admin/homes");
     }
-  }, [token]);
+  }, [token, navigate]);
 
   const handleLogin = async () => {
     if (!userData.username || !userData.password || loading) return;
+
     const res = await login(userData);
+
     if (res?.data?.access_token) {
       navigate("/admin/homes");
     }
@@ -33,32 +40,34 @@ const Login = () => {
   return (
     <div className={styles.imagebackground}>
       <div>
-        <h1>Մուտք</h1>
+        <h1>{t("admin.login.title")}</h1>
 
         <input
           className={styles.input}
-          placeholder="Մուտքագրեք էլ. հասցե կամ օգտանուն"
+          placeholder={t("admin.login.usernamePlaceholder")}
           type="text"
           value={userData.username}
           onChange={(e) =>
             setUserData({ ...userData, username: e.target.value })
           }
         />
+
         <div style={{ position: "relative" }}>
           <input
             className={styles.input}
-            placeholder="Մուտքագրեք գաղտնաբառ"
+            placeholder={t("admin.login.passwordPlaceholder")}
             type={showPassword ? "text" : "password"}
             value={userData.password}
             onChange={(e) =>
               setUserData({ ...userData, password: e.target.value })
             }
           />
+
           <div
             onClick={() => setShowPassword(!showPassword)}
             className={styles.passwordIcon}
           >
-            <img src={closeEye} alt="Toggle visibility" />
+            <img src={closeEye} alt="Toggle password visibility" />
           </div>
         </div>
 
@@ -69,7 +78,7 @@ const Login = () => {
           onClick={handleLogin}
           disabled={loading}
         >
-          {loading ? "Բեռնում..." : "Մուտք"}
+          {loading ? t("admin.login.loading") : t("admin.login.button")}
         </button>
       </div>
     </div>
